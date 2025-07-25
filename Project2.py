207b65b1eb87b58873deec6d50b8d029b9467a1b
funkycoder36 <ifti80@yahoo.com>
2025-07-25 20:59:47 +0500

    Project2

---
+++ Project2.py
@@ -0,0 +1,23 @@
+name = "penguin"
+age = "15"
+is_student = True
+weight = 34.5
+
+print("Name :", name)
+print("Data type of name is ", type(name))
+
+print("Age : ", name)
+print("Data type of name is ", type(age))
+
+print("is_student : ", is_student)
+print("Data type of name is ", type(is_student))
+
+print("Weight : ", weight)
+print("Data type of name is ", type(weight))
+
+age = str(age)
+print(age)
+print("Data type of name is ", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of name is ", type(weight))
