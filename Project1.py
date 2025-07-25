6b0ed4e8922b7bf8dab1d2fa98076bc3fe810df0
funkycoder36 <ifti80@yahoo.com>
2025-07-25 20:49:08 +0500

    Project1

---
+++ Project1.py
@@ -0,0 +1,9 @@
+# lets check the datatype of different values
+a = 5
+print("type of a: ", type(a))
+b = 2.4
+print("type of b: ", type(b))
+c = "hello"
+print("type of c: ", type(c))
+d = True
+print("type of d: ", type(d))
